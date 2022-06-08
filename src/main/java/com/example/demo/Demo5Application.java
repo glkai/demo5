fd119cd993bf3f1e8f2eae9d9fcc9da729a86d65
glKai <liukgao@163.com>
2022-06-08 13:05:07 +0800

    mas

--- src/main/java/com/example/demo/Demo5Application.java
+++ src/main/java/com/example/demo/Demo5Application.java
@@ -10,6 +10,7 @@ public class Demo5Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Demo5Application.class, args);
+        System.out.println(10);
     }
 
 }
