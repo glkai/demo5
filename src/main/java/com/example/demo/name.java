75be57796a858261dad389f338633da87b31abce
glKai <liukgao@163.com>
2022-06-08 16:44:43 +0800

    mas

---
+++ src/main/java/com/example/demo/name.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Data
+@AllArgsConstructor
+public class name {
+    private String name;
+}
